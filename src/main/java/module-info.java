c4f3e27313afc1aafbd9110446a45abc578c7fcc
kacperchlastawa <kacper.chlastawa03@gmail.com>
2025-11-11 19:38:19 +0100

    first actions

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module pl.dice_game.java_dice_game {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-
-    opens pl.dice_game.java_dice_game to javafx.fxml;
-    exports pl.dice_game.java_dice_game;
-}
